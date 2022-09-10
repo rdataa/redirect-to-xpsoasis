827a5a084f5628ce99bb5c7ec886dd396d036aec
oswaldo <oscargallos99@gmail.com>
2022-09-10 12:01:41 -0500

    Redirect project ready

---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import           "redirect-to-xpsoasis" Application (develMain)
+import           Prelude                            (IO)
+
+main :: IO ()
+main = develMain
