2dde33300071019bfe87f74a1817e5c1f119b2c9
OMOPER <oscargallos99@gmail.com>
2022-12-15 21:57:51 -0600

    Refactor

--- src/Handler/Common.hs
+++ src/Handler/Common.hs
@@ -1,16 +1,14 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 -- | Common handler functions.
 module Handler.Common where
 
-import Data.FileEmbed (embedFile)
-import Import
+import           Data.FileEmbed (embedFile)
+import           Import
 
--- These handlers embed files in the executable at compile time to avoid a
--- runtime dependency, and for efficiency.
 
 getFaviconR :: Handler TypedContent
 getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
